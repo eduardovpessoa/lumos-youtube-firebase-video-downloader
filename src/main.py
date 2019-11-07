84b4bc360c2346dcf61c9eddc8ed23db9ecf2f0b
Eduardo Viana Pessoa <eduardo.vianap@gmail.com>
2019-11-07 12:01:45 -0700

    Add. routine to delete videos.

--- src/main.py
+++ src/main.py
@@ -1,28 +1,39 @@
 import json
-import youtube_dl
 import pyrebase
+import os
+import os.path
+import youtube_dl
 
 def main(config):
+    tv = "first_tv"
+    #tv = "second_tv"
+    save_path = 'C:\\Users\\epires\\Desktop\\'
     firebase = pyrebase.initialize_app(config)
     db = firebase.database()
-    new_videos = db.child("videos").order_by_child("first_tv").equal_to(0).get()
-    print(new_videos)
-    for video in new_videos.each():
-        print(video.key())
-        print(video.val()["url"])
-        download(video.key(), video.val()["url"])
     
+    new_videos = db.child("videos").order_by_child(tv).equal_to(0).get() #Query to list new videos
+    for new in new_videos.each():
+        print("Downloading...: " + new.val()["name"])
+        download(new.key(), new.val()["url"], save_path) #Download video
+        db.child("videos").child(new.key()).update({tv: 1}) #Update video status
 
-def download(id, url):
-    SAVE_PATH = 'C:\\Users\\eduardo\\Desktop\\'
+    old_videos = db.child("videos").order_by_child(tv).equal_to(2).get() #Query to list old videos
+    for old in old_videos.each():
+        delete_path = save_path + old.key() + ".mp4"
+        if os.path.exists(delete_path):
+            print("Removing...: " + old.val()["name"])
+            os.remove(delete_path)
+            db.child("videos").child(old.key()).update({tv: 3}) #Update video status
+        else:
+            print("Video not found! \nName:" + old.val()["name"] + "\nFile: " + delete_path + "\n")
+    
+def download(id, url, save_path):
     ydl_opts = {
         'format': 'best',
-        'outtmpl': SAVE_PATH + id + '.%(ext)s',
+        'outtmpl': save_path + id + '.%(ext)s',
     }
-    print(ydl_opts)
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-    print("Baixou?")
 
 if __name__ == '__main__':
     
